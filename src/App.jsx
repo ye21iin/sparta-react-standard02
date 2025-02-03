d4bd122c8612fe355533f56854a60d7a27ab4731
ye21iin <kathykr120@gmail.com>
2025-02-03 22:17:34 +0900

    Feat: update DELETE / UPDATE - complete delete feature - update feature: move todo selected into cleared state

--- src/App.jsx
+++ src/App.jsx
@@ -100,7 +100,16 @@ function App() {
     const deletedTodos = subject.filter((el) => {
       return el.id !== id;
     });
-    setTodos(deletedTodos);
+    subject === todos ? setTodos(deletedTodos) : setCleared(deletedTodos);
+  };
+
+  const handleComplete = (id) => {
+    const completed = todos.find((x) => x.id === id);
+    const newTodos = todos.filter((el) => {
+      return el.id !== id;
+    });
+    setTodos(newTodos);
+    setCleared([...cleared, completed]);
   };
 
   return (
@@ -134,7 +143,7 @@ function App() {
               <Box key={todo.id}>
                 <h4>{todo.title}</h4>
                 <p>{todo.contents}</p>
-                <Button>완료</Button>
+                <Button onClick={() => handleComplete(todo.id)}>완료</Button>
                 <DeleteBtn onClick={() => handleDelete(todo.id)}>
                   삭제하기
                 </DeleteBtn>
